4fe99293de954d6dcee4cd26dc2adc6c10e8fcda
mateuspedro4 <61235092+mateuspedro4@users.noreply.github.com>
2021-09-03 18:55:27 -0300

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,4 @@
+<?php 
+  echo "<pre><h2>Infraestrutura PHP e MySQL.</h2></pre><br/>";
+  mysqli_connect("db", "root", "GAud4mZby8F3SD6P") or die(mysqli_error()); 
+  echo "<pre><h3>Connected to MySQL<h3></pre><br/>";
